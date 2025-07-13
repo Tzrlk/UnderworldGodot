6b8973f2e1d6ec1e5ecdaffdc4d719cac1829219
tzrlk <peterc@aetheric.co.nz>
2025-07-13 16:40:54 +1200

    Making all known Nodes injectable.

--- src/utility/LaunchScene.cs
+++ src/utility/LaunchScene.cs
@@ -1,11 +1,16 @@
+using Chickensoft.AutoInject;
+using Chickensoft.Introspection;
 using Godot;
 
 namespace Underworld
 {
+	[Meta(typeof(IAutoNode))]
     public partial class LaunchScene : Node
 	{
-		// Called when the node enters the scene tree for the first time.
-		public override void _Ready()
+        public override void _Notification(int what) => this.Notify(what);
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
 		{
 			GD.Print("Launch scene now loading Underworld.tscn.");
 			GetTree().ChangeSceneToFile("res://Underworld.tscn");
